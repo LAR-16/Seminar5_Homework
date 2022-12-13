dbedbda82a75fbf0131c3a1d9a3c4790b47d08f2
Alexandra <lebedeva1618@yandex.ru>
2022-12-13 14:18:11 +0300

    задача 36, сумма элементов под нечетным индексом

---
+++ task36/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputArray (int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11);
+}
+
+void ReleaseArray (int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i=i+2)
+        sum = sum + array[i];
+    Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях = {sum}");
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
